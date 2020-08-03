9c4d3df3fcbf80fe4f75511f2091919843b09c00
Rodney Beede <rbeede@users.noreply.github.com>
2020-08-03 11:15:33 -0500

    More handy scripts for pen test setup

--- setup-aws-cli.bash
+++ setup-aws-cli.bash
@@ -4,9 +4,10 @@ set -o xtrace
 
 echo You need to  source  this
 
+# https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-proxy.html
+# Must be upper-case names
 export HTTPS_PROXY=http://127.0.0.1:8080
-
-export HTTP_PROXY=http://127.0.0.1:8080
+export HTTP_PROXY=$HTTPS_PROXY
 
 
 openssl x509 -inform der -in ~/Documents/burp-ca.crt -out ~/Documents/burp.pem
