19abf572332f402dd386c56b2994b916d8c2e46b
Rodney Beede <rbeede@users.noreply.github.com>
2020-08-06 16:10:11 -0500

    New file structure

---
+++ roles/awscli/files/setup-aws-cli.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o xtrace
+
+echo You need to  source  this
+
+# https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-proxy.html
+# Must be upper-case names
+export HTTPS_PROXY=http://127.0.0.1:8080
+export HTTP_PROXY=$HTTPS_PROXY
+
+
+export AWS_CA_BUNDLE=~/Documents/burp-ca.pem
+
+export NO_PROXY=169.254.169.254
+
+aws --version
+
+aws configure list-profiles
+
+set +o xtrace
