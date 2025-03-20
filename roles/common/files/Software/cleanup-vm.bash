e2fefc36accfc18599afdf07db394921f613f790
Rodney Beede <rbeede@users.noreply.github.com>
2025-03-20 07:28:30 -0500

    fstrim

--- roles/common/files/Software/cleanup-vm.bash
+++ roles/common/files/Software/cleanup-vm.bash
@@ -11,14 +11,13 @@ rm -Rf ~/.cache/mozilla
 
 sudo rm /root/.bash_history
 
+rm ~/.bash_history
+rm ~/.lesshst ~/.viminfo
+
 rm -Rf ~/.aws/
 
 # no secrets in the family
 find ~/.ssh -type f \! \( -name known_hosts -o -name config \) -exec rm -f {} \;
-
-
-rm ~/.bash_history
-rm ~/.lesshst ~/.viminfo
 
 kdestroy -A
 
@@ -28,6 +27,8 @@ sudo apt-get clean
 sudo find /var/cache/cinmn/ -type f -exec rm "{}" \;
 
 # Clean out logs
+sudo journalctl --vacuum-size=1M
+
 sudo find /var/log -path /var/log/journal -prune -o -type f -name '*.xz' -o -name '*.gz' -exec rm {} \;
 
 sudo find /var/log -path /var/log/journal -prune -o -type f -name '*.[0-9]' -exec rm {} \;
@@ -38,6 +39,7 @@ sudo find /var/log -path /var/log/journal -prune -o -type f -exec truncate --no-
 
 sudo journalctl --vacuum-size=1M
 
+sudo fstrim --verbose --all
 
 # Show any personal files that may still be around
 find ~/Downloads ~/Desktop
